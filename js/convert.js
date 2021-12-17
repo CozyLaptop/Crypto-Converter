a47012a611ce710bac9d1b60e558f4960474f69c
Albert Dominguez <albert.j.dominguez21@gmail.com>
2021-12-17 08:53:40 -0600

    refactor and added comments

--- js/convert.js
+++ js/convert.js
@@ -1,47 +1,56 @@
-    const url = "https://api.coingecko.com/api/v3/";
-    var coinArray = [];
+const url = "https://api.coingecko.com/api/v3/";
+var coinArray = [];
+let currentCoinPrice;
 
-    let currentBnbPrice;
-    function refreshBNBPrice(id) {
+//Refreshes the coin price on a set interval
+function refreshBNBPrice(id) {
     fetch(url + "/coins/" + id).then(response => {
         response.json().then(object => {
-            currentBnbPrice = object.market_data.current_price.usd;
-            console.log(currentBnbPrice)
-            $(".currentCryptoPrice").text(currentBnbPrice);
+            currentCoinPrice = object.market_data.current_price.usd;
+            console.log(currentCoinPrice)
+            $(".currentCryptoPrice").text(currentCoinPrice);
         });
     });
 }
-    function loadTop100CG(){
+//10 second interval refresh
+setInterval(refreshBNBPrice, 5 * 1000, id = "binancecoin");
+
+//Loads the top 100 coins (by marketcap) into the global array
+function loadTop100CG(){
     var currency = "vs_currency=usd";
     fetch(url + "/coins/markets?" + currency).then(response=>{response.json().then(object=>{
-    object.forEach(coin => {
-    coinArray.push(coin);
-});
-}).then(()=>{
-    console.log(coinArray);
-});
-});
+        object.forEach(coin => {
+            coinArray.push(coin);
+        });
+    }).then(()=>{
+        console.log(coinArray);
+    });
+    });
 }
-    function onLoadDefaultValues(id){
+
+//    This function will load default values on page load
+//    Currently is set for BNB
+function onLoadDefaultValues(id){
     fetch(url + "/coins/" + id).then(response=>{response.json().then(object=>{
-        currentBnbPrice = object.market_data.current_price.usd;
-        $(".currentCryptoPrice").text(currentBnbPrice);
+        currentCoinPrice = object.market_data.current_price.usd;
+        $(".currentCryptoPrice").text(currentCoinPrice);
         //Left input
         document.getElementById("inputCryptoValue").value = 1;
         //Right output
-        document.getElementById("outputCryptoValue").value = currentBnbPrice;
+        document.getElementById("outputCryptoValue").value = currentCoinPrice;
     });
     });
 }
-    onLoadDefaultValues("binancecoin");
 
-    //10 second interval refresh
-    setInterval(refreshBNBPrice, 5 * 1000, id = "binancecoin");
 
-    //Events
-    $(".convertButton").click(()=>{
+//Events
+//Upon clicking convert button, will calculate the conversion
+$(".convertButton").click(()=>{
     let inputNum = document.getElementById("inputCryptoValue").value;
-    document.getElementById("outputCryptoValue").value = Number(currentBnbPrice * inputNum).toFixed(2);
+    document.getElementById("outputCryptoValue").value = Number(currentCoinPrice * inputNum).toFixed(2);
 });
 
-    loadTop100CG();
+//Init
+loadTop100CG();
+onLoadDefaultValues("binancecoin");
+
